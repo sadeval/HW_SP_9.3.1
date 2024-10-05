8a89b95e71a1f322d76af878758340465211ddac
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-05 12:04:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TextUtilsLibrary;
+
+namespace TextApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст для проверки на палиндром:");
+            string? inputText = Console.ReadLine();
+
+            // Проверка на палиндром
+            bool isPalindrome = TextUtils.IsPalindrome(inputText);
+            Console.WriteLine($"Является ли палиндромом: {isPalindrome}");
+
+            // Подсчет количества предложений
+            int sentenceCount = TextUtils.CountSentences(inputText);
+            Console.WriteLine($"Количество предложений: {sentenceCount}");
+
+            // Переворот строки
+            string reversedText = TextUtils.ReverseString(inputText);
+            Console.WriteLine($"Перевернутая строка: {reversedText}");
+
+            Console.ReadLine();
+        }
+    }
+}
